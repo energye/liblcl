79e4b2e0b331eb76ca3869257c2317eed2623410
杨红岩 <snxamdf@126.com>
2023-02-25 20:08:45 +0800

    升级和优化，绑定和进程通信 v9

--- src/uCEF_LCL_V8InterceptorRef.pas
+++ src/uCEF_LCL_V8InterceptorRef.pas
@@ -38,6 +38,7 @@ implementation
 
 function TV8InterceptorRef.GetByName(const Name: ustring; const object_: ICefv8Value; var retval: ICefv8Value; var Exception: ustring): boolean;
 begin
+  Result := False;
   if (GetByNamePtr <> nil) then
   begin
     SendEvent(GetByNamePtr, []);
@@ -46,6 +47,7 @@ end;
 
 function TV8InterceptorRef.GetByIndex(index: integer; const object_: ICefv8Value; var retval: ICefv8Value; var Exception: ustring): boolean;
 begin
+  Result := False;
   if (GetByIndexPtr <> nil) then
   begin
     SendEvent(GetByIndexPtr, []);
@@ -54,6 +56,7 @@ end;
 
 function TV8InterceptorRef.SetByName(const Name: ustring; const object_, Value: ICefv8Value; var Exception: ustring): boolean;
 begin
+  Result := False;
   if (SetByNamePtr <> nil) then
   begin
     SendEvent(SetByNamePtr, []);
@@ -62,6 +65,7 @@ end;
 
 function TV8InterceptorRef.SetByIndex(index: integer; const object_, Value: ICefv8Value; var Exception: ustring): boolean;
 begin
+  Result := False;
   if (SetByIndexPtr <> nil) then
   begin
     SendEvent(SetByIndexPtr, []);
