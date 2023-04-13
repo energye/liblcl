9c799df7f7e1311845b81cb8819cb6fe83464f4a
杨红岩 <snxamdf@126.com>
2023-04-13 13:15:16 +0800

    upgrade-dev

---
+++ src/uCEF_LCL_CompletionCallbackRef.pas
@@ -0,0 +1,50 @@
+//----------------------------------------
+// Copyright © yanghy. All Rights Reserved.
+
+// Licensed under Lazarus.modifiedLGPL
+//----------------------------------------
+
+unit uCEF_LCL_CompletionCallbackRef;
+
+{$mode objfpc}{$H+}
+{$I cef.inc}
+
+interface
+
+uses
+  uCEFTypes, uCEFCompletionCallback,
+  uCEF_LCL_EventCallback;
+
+type
+
+  TCompletionCallbackRef = class(TCefCompletionCallbackOwn)
+  public
+    CompletionCallbackPtr: Pointer;
+    constructor Create;
+    destructor Destroy;
+  protected
+    procedure OnComplete(); override;
+
+  end;
+
+implementation
+
+procedure TCompletionCallbackRef.OnComplete();
+begin
+  if (CompletionCallbackPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(CompletionCallbackPtr, []);
+  end;
+end;
+
+constructor TCompletionCallbackRef.Create;
+begin
+  inherited Create;
+end;
+
+destructor TCompletionCallbackRef.Destroy;
+begin
+  inherited Destroy;
+end;
+
+end.
