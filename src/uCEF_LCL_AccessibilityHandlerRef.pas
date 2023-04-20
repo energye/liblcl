956123b48411a3ef618f8fc81d1534179e8249e0
杨红岩 <snxamdf@126.com>
2023-04-20 11:26:11 +0800

    upgrade-dev

---
+++ src/uCEF_LCL_AccessibilityHandlerRef.pas
@@ -0,0 +1,62 @@
+//----------------------------------------
+// Copyright © yanghy. All Rights Reserved.
+
+// Licensed under Lazarus.modifiedLGPL
+//----------------------------------------
+
+unit uCEF_LCL_AccessibilityHandlerRef;
+
+{$mode objfpc}{$H+}
+{$I cef.inc}
+
+interface
+
+uses
+  uCEFTypes, uCEFInterfaces, uCEFAccessibilityHandler,
+  uCEF_LCL_EventCallback;
+
+type
+
+  TAccessibilityHandlerRef = class(TCEFAccessibilityHandlerOwn)
+  public
+    AccessibilityTreeChangePtr: Pointer;
+    AccessibilityLocationChangePtr: Pointer;
+    constructor Create; override;
+    destructor Destroy;
+  protected
+    procedure OnAccessibilityTreeChange(const Value: ICefValue); override;
+    procedure OnAccessibilityLocationChange(const Value: ICefValue); override;
+
+  end;
+
+implementation
+
+procedure TAccessibilityHandlerRef.OnAccessibilityTreeChange(const Value: ICefValue);
+begin
+  if (AccessibilityTreeChangePtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(AccessibilityTreeChangePtr, [Value]);
+  end;
+end;
+
+procedure TAccessibilityHandlerRef.OnAccessibilityLocationChange(const Value: ICefValue);
+begin
+  if (AccessibilityLocationChangePtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(AccessibilityLocationChangePtr, [Value]);
+  end;
+end;
+
+constructor TAccessibilityHandlerRef.Create;
+begin
+  inherited Create;
+end;
+
+destructor TAccessibilityHandlerRef.Destroy;
+begin
+  AccessibilityTreeChangePtr := nil;
+  AccessibilityLocationChangePtr := nil;
+  inherited Destroy;
+end;
+
+end.
