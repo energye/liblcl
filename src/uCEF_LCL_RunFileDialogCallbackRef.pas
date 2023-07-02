46f7f2b87a0396737926548b1705564f9ed0f8c2
杨红岩 <snxamdf@126.com>
2023-07-02 10:57:34 +0800

    A: Browser RunFileDialog CallbackFunc

---
+++ src/uCEF_LCL_RunFileDialogCallbackRef.pas
@@ -0,0 +1,54 @@
+//----------------------------------------
+// Copyright © yanghy. All Rights Reserved.
+
+// Licensed under Lazarus.modifiedLGPL
+//----------------------------------------
+
+unit uCEF_LCL_RunFileDialogCallbackRef;
+
+{$mode objfpc}{$H+}
+{$I cef.inc}
+
+interface
+
+uses
+  Classes, SysUtils,
+  uCEFRunFileDialogCallback,
+  uCEF_LCL_EventCallback;
+
+type
+
+  TRunFileDialogCallbackRef = class(TCefRunFileDialogCallbackOwn)
+  public
+    FileDialogDismissedPtr: Pointer;
+    constructor Create;
+    destructor Destroy;
+  protected
+    procedure OnFileDialogDismissed(const filePaths: TStrings); override;
+
+  end;
+
+implementation
+
+procedure TRunFileDialogCallbackRef.OnFileDialogDismissed(const filePaths: TStrings);
+begin
+  if (FileDialogDismissedPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(FileDialogDismissedPtr, [filePaths]);
+  end
+  else
+    inherited OnFileDialogDismissed(filePaths);
+end;
+
+constructor TRunFileDialogCallbackRef.Create;
+begin
+  inherited Create;
+end;
+
+destructor TRunFileDialogCallbackRef.Destroy;
+begin
+  inherited Destroy;
+  FileDialogDismissedPtr := nil;
+end;
+
+end.
