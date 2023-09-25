9afa2b3266434e9ead22d28a6a8187051bd020a4
杨红岩 <snxamdf@126.com>
2023-09-25 10:25:53 +0800

    移除 ConsoleLn 函数

--- src/uCEF_LCL_ConsoleWrite.pas
+++ src/uCEF_LCL_ConsoleWrite.pas
@@ -8,17 +8,12 @@ unit uCEF_LCL_ConsoleWrite;
 
 {$mode objfpc}{$H+}
 {$I cef.inc}
-{$APPTYPE CONSOLE}
+//{$APPTYPE CONSOLE}
 
 interface
 
-procedure ConsoleLn(msg: UnicodeString);
 
 implementation
 
-procedure ConsoleLn(msg: UnicodeString);
-begin
-  //WriteLn('==[CEF-LCL-Log] == [', msg,']');
-end;
 
 end.
