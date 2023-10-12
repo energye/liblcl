f2784571e11e4bb987c04abe32a41dfd13ae637b
杨红岩 <snxamdf@126.com>
2023-10-12 12:26:17 +0800

    升级 liblcl CEF 117.2.4

--- src/uCEF_LCL_BrowserViewDelegateRef.pas
+++ src/uCEF_LCL_BrowserViewDelegateRef.pas
@@ -21,18 +21,24 @@ type
 
   TBrowserViewDelegateRef = class(TCefBrowserViewDelegateOwn)
   public
+    // ptr
     BrowserCreatedPtr: Pointer;
     BrowserDestroyedPtr: Pointer;
     GetDelegateForPopupBrowserViewPtr: Pointer;
     PopupBrowserViewCreatedPtr: Pointer;
+    GetChromeToolbarTypePtr: Pointer;
+    UseFramelessWindowForPictureInPicturePtr: Pointer;
+    GestureCommandPtr: Pointer;
+    // override
     constructor Create; override;
     destructor Destroy; override;
-    function  GetChromeToolbarType: TCefChromeToolbarType; override;
-  protected
     procedure OnBrowserCreated(const browser_view: ICefBrowserView; const browser: ICefBrowser); override;
     procedure OnBrowserDestroyed(const browser_view: ICefBrowserView; const browser: ICefBrowser); override;
     procedure OnGetDelegateForPopupBrowserView(const browser_view: ICefBrowserView; const settings: TCefBrowserSettings; const client: ICefClient; is_devtools: boolean; var aResult: ICefBrowserViewDelegate); override;
     procedure OnPopupBrowserViewCreated(const browser_view, popup_browser_view: ICefBrowserView; is_devtools: boolean; var aResult: boolean); override;
+    procedure OnGetChromeToolbarType(const browser_view: ICefBrowserView; var aResult : TCefChromeToolbarType); override;
+    procedure OnUseFramelessWindowForPictureInPicture(const browser_view: ICefBrowserView; var aResult: boolean); override;
+    procedure OnGestureCommand(const browser_view: ICefBrowserView; gesture_command: TCefGestureCommand; var aResult : boolean); override;
     procedure InitializeCEFMethods; override;
 
   end;
@@ -78,9 +84,28 @@ begin
   end;
 end;
 
-function TBrowserViewDelegateRef.GetChromeToolbarType: TCefChromeToolbarType;
+procedure TBrowserViewDelegateRef.OnGetChromeToolbarType(const browser_view: ICefBrowserView; var aResult : TCefChromeToolbarType);
 begin
-  Result := inherited GetChromeToolbarType;
+  if (GetChromeToolbarTypePtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(GetChromeToolbarTypePtr, [browser_view, @aResult]);
+  end;
+end;
+
+procedure TBrowserViewDelegateRef.OnUseFramelessWindowForPictureInPicture(const browser_view: ICefBrowserView; var aResult: boolean);
+begin
+  if (UseFramelessWindowForPictureInPicturePtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(UseFramelessWindowForPictureInPicturePtr, [browser_view, @aResult]);
+  end;
+end;
+
+procedure TBrowserViewDelegateRef.OnGestureCommand(const browser_view: ICefBrowserView; gesture_command: TCefGestureCommand; var aResult : boolean);
+begin
+  if (GestureCommandPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(GestureCommandPtr, [browser_view, gesture_command, @aResult]);
+  end;
 end;
 
 procedure TBrowserViewDelegateRef.InitializeCEFMethods;
@@ -96,6 +121,9 @@ begin
   BrowserDestroyedPtr := nil;
   GetDelegateForPopupBrowserViewPtr := nil;
   PopupBrowserViewCreatedPtr := nil;
+  GetChromeToolbarTypePtr := nil;
+  UseFramelessWindowForPictureInPicturePtr := nil;
+  GestureCommandPtr := nil;
 end;
 
 destructor TBrowserViewDelegateRef.Destroy;
@@ -105,6 +133,9 @@ begin
   BrowserDestroyedPtr := nil;
   GetDelegateForPopupBrowserViewPtr := nil;
   PopupBrowserViewCreatedPtr := nil;
+  GetChromeToolbarTypePtr := nil;
+  UseFramelessWindowForPictureInPicturePtr := nil;
+  GestureCommandPtr := nil;
 end;
 
 end.
