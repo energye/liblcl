30782adafa6dbbb993b3cea07e893ca63de625af
杨红岩 <snxamdf@126.com>
2024-01-07 20:38:13 +0800

    U: 修改 CEF types delegate 相关组件事件, 使用自己实现的事件

--- src/uCEF_LCL_PanelDelegateRef.pas
+++ src/uCEF_LCL_PanelDelegateRef.pas
@@ -12,7 +12,7 @@ unit uCEF_LCL_PanelDelegateRef;
 interface
 
 uses
-  Classes, SysUtils,uCEFInterfaces, uCEFTypes,
+  Classes, SysUtils, uCEFInterfaces, uCEFTypes,
   uCEFPanelDelegate,
   uCEF_LCL_EventCallback;
 
@@ -20,22 +20,146 @@ type
 
   TPanelDelegateRef = class(TCefPanelDelegateOwn)
   public
+    // ICefViewDelegate
+    GetPreferredSizePtr: Pointer;
+    GetMinimumSizePtr: Pointer;
+    GetMaximumSizePtr: Pointer;
+    GetHeightForWidthPtr: Pointer;
+    ParentViewChangedPtr: Pointer;
+    ChildViewChangedPtr: Pointer;
+    WindowChangedPtr: Pointer;
+    LayoutChangedPtr: Pointer;
+    FocusPtr: Pointer;
+    BlurPtr: Pointer;
+
     constructor Create; override;
     destructor Destroy; override;
   protected
-
+    // ICefViewDelegate
+    procedure OnGetPreferredSize(const view: ICefView; var aResult: TCefSize); override;
+    procedure OnGetMinimumSize(const view: ICefView; var aResult: TCefSize); override;
+    procedure OnGetMaximumSize(const view: ICefView; var aResult: TCefSize); override;
+    procedure OnGetHeightForWidth(const view: ICefView; Width: integer; var aResult: integer); override;
+    procedure OnParentViewChanged(const view: ICefView; added: boolean; const parent: ICefView); override;
+    procedure OnChildViewChanged(const view: ICefView; added: boolean; const child: ICefView); override;
+    procedure OnWindowChanged(const view: ICefView; added: boolean); override;
+    procedure OnLayoutChanged(const view: ICefView; new_bounds: TCefRect); override;
+    procedure OnFocus(const view: ICefView); override;
+    procedure OnBlur(const view: ICefView); override;
   end;
 
 implementation
 
+
+// ICefViewDelegate
+procedure TPanelDelegateRef.OnGetPreferredSize(const view: ICefView; var aResult: TCefSize);
+begin
+  if (GetPreferredSizePtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(GetPreferredSizePtr, [view, @aResult]);
+  end;
+end;
+
+procedure TPanelDelegateRef.OnGetMinimumSize(const view: ICefView; var aResult: TCefSize);
+begin
+  if (GetMinimumSizePtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(GetMinimumSizePtr, [view, @aResult]);
+  end;
+end;
+
+procedure TPanelDelegateRef.OnGetMaximumSize(const view: ICefView; var aResult: TCefSize);
+begin
+  if (GetMaximumSizePtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(GetMaximumSizePtr, [view, @aResult]);
+  end;
+end;
+
+procedure TPanelDelegateRef.OnGetHeightForWidth(const view: ICefView; Width: integer; var aResult: integer);
+begin
+  if (GetHeightForWidthPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(GetHeightForWidthPtr, [view, Width, @aResult]);
+  end;
+end;
+
+procedure TPanelDelegateRef.OnParentViewChanged(const view: ICefView; added: boolean; const parent: ICefView);
+begin
+  if (ParentViewChangedPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(ParentViewChangedPtr, [view, added, parent]);
+  end;
+end;
+
+procedure TPanelDelegateRef.OnChildViewChanged(const view: ICefView; added: boolean; const child: ICefView);
+begin
+  if (ChildViewChangedPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(ChildViewChangedPtr, [view, added, child]);
+  end;
+end;
+
+procedure TPanelDelegateRef.OnWindowChanged(const view: ICefView; added: boolean);
+begin
+  if (WindowChangedPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(WindowChangedPtr, [view, added]);
+  end;
+end;
+
+procedure TPanelDelegateRef.OnLayoutChanged(const view: ICefView; new_bounds: TCefRect);
+begin
+  if (LayoutChangedPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(LayoutChangedPtr, [view, @new_bounds]);
+  end;
+end;
+
+procedure TPanelDelegateRef.OnFocus(const view: ICefView);
+begin
+  if (FocusPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(FocusPtr, [view]);
+  end;
+end;
+
+procedure TPanelDelegateRef.OnBlur(const view: ICefView);
+begin
+  if (BlurPtr <> nil) then
+  begin
+    TCEFEventCallback.SendEvent(BlurPtr, [view]);
+  end;
+end;
+
 constructor TPanelDelegateRef.Create;
 begin
   inherited Create;
+  GetPreferredSizePtr := nil;
+  GetMinimumSizePtr := nil;
+  GetMaximumSizePtr := nil;
+  GetHeightForWidthPtr := nil;
+  ParentViewChangedPtr := nil;
+  ChildViewChangedPtr := nil;
+  WindowChangedPtr := nil;
+  LayoutChangedPtr := nil;
+  FocusPtr := nil;
+  BlurPtr := nil;
 end;
 
 destructor TPanelDelegateRef.Destroy;
 begin
   inherited Destroy;
+  GetPreferredSizePtr := nil;
+  GetMinimumSizePtr := nil;
+  GetMaximumSizePtr := nil;
+  GetHeightForWidthPtr := nil;
+  ParentViewChangedPtr := nil;
+  ChildViewChangedPtr := nil;
+  WindowChangedPtr := nil;
+  LayoutChangedPtr := nil;
+  FocusPtr := nil;
+  BlurPtr := nil;
 end;
 
 end.
