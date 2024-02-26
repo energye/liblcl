79a303bbb67ced600e1c491a2267af455b8d01b1
yanghy <snxamdf@126.com>
2024-02-26 14:59:31 +0800

    Linux编译问题，去除MyLCL_IniFile.inc
    (cherry picked from commit 1ab8d39c826f4569fedce9d21bcac1b1d8744e19)
    
    (cherry picked from commit bca50227404e0f2c09b6457c93c2d2c1f4af6acd)

--- src/3rd-party/CEF4Delphi/source/uCEFLinuxFunctions.pas
+++ src/3rd-party/CEF4Delphi/source/uCEFLinuxFunctions.pas
@@ -1,50 +1,13 @@
-// ************************************************************************
-// ***************************** CEF4Delphi *******************************
-// ************************************************************************
-//
-// CEF4Delphi is based on DCEF3 which uses CEF to embed a chromium-based
-// browser in Delphi applications.
-//
-// The original license of DCEF3 still applies to CEF4Delphi.
-//
-// For more information about CEF4Delphi visit :
-//         https://www.briskbard.com/index.php?lang=en&pageid=cef
-//
-//        Copyright © 2021 Salvador Diaz Fau. All rights reserved.
-//
-// ************************************************************************
-// ************ vvvv Original license and comments below vvvv *************
-// ************************************************************************
-(*
- *                       Delphi Chromium Embedded 3
- *
- * Usage allowed under the restrictions of the Lesser GNU General Public License
- * or alternatively the restrictions of the Mozilla Public License 1.1
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * Unit owner : Henri Gourvest <hgourvest@gmail.com>
- * Web site   : http://www.progdigy.com
- * Repository : http://code.google.com/p/delphichromiumembedded/
- * Group      : http://groups.google.com/group/delphichromiumembedded
- *
- * Embarcadero Technologies, Inc is not permitted to use or redistribute
- * this source code without explicit permission.
- *
- *)
-
 unit uCEFLinuxFunctions;
 
 {$IFDEF FPC}
   {$MODE OBJFPC}{$H+}
 {$ENDIF}
 
-{$IFNDEF CPUX64}{$ALIGN ON}{$ENDIF}
-{$MINENUMSIZE 4}
-
 {$I cef.inc}
+
+{$IFNDEF TARGET_64BITS}{$ALIGN ON}{$ENDIF}
+{$MINENUMSIZE 4}
 
 interface
 
@@ -53,6 +16,7 @@ uses
     {$IFDEF FPC}
       ctypes, keysym, xf86keysym, x, xlib,
       {$IFDEF LCLGTK2}gtk2, glib2, gdk2, gtk2proc, gtk2int, Gtk2Def, gdk2x, Gtk2Extra,{$ENDIF}
+      {$IFDEF LCLGTK3}LazGdk3, LazGtk3, LazGObject2, LazGLib2, gtk3objects, gtk3procs,{$ENDIF}
     {$ENDIF}
   {$ENDIF}
   uCEFLinuxTypes, uCEFTypes;
@@ -64,13 +28,30 @@ function  GetCefStateModifiers(state : uint32) : integer;
 function  GdkEventToWindowsKeyCode(Event: PGdkEventKey) : integer;
 function  GetWindowsKeyCodeWithoutLocation(key_code : integer) : integer;
 function  GetControlCharacter(windows_key_code : integer; shift : boolean) : integer;
+
 {$IFDEF FMX}
+type
+   TXErrorHandler   = function (para1:PDisplay; para2:PXErrorEvent):longint; cdecl;
+   TXIOErrorHandler = function (para1:PDisplay):longint; cdecl;
+
+function XSetErrorHandler(para1:TXErrorHandler):TXErrorHandler; cdecl; external 'libX11.so';
+function XSetIOErrorHandler(para1:TXIOErrorHandler):TXIOErrorHandler; cdecl; external 'libX11.so';
+
 function gdk_keyval_to_unicode(keyval: guint): guint32; cdecl; external 'libgdk-3.so';
 function g_signal_connect_data(instance: gpointer; detailed_signal: Pgchar; c_handler: TGCallback; data: gpointer; destroy_data: TGClosureNotify; connect_flags: TGConnectFlags): gulong; cdecl; external 'libgobject-2.0.so';
 function g_signal_connect(instance: gpointer; detailed_signal: Pgchar; c_handler: TGCallback; data: gpointer): gulong; overload;
 function g_signal_connect(instance: gpointer; const detailed_signal: AnsiString; c_handler: TGCallback; data: gpointer): gulong; overload;
+function gdk_screen_width:gint; cdecl; external 'libgdk-3.so';
+function gdk_screen_width_mm:gint; cdecl; external 'libgdk-3.so';
+function gdk_screen_get_default:PGdkScreen; cdecl; external 'libgdk-3.so';
+function gdk_screen_get_resolution(screen:PGdkScreen):gdouble; cdecl; external 'libgdk-3.so';
 {$ENDIF}
 {$IFDEF FPC}
+{$IFDEF LCLGTK3}
+function gdk_x11_window_get_xid(window: PGdkWindow): TXID; cdecl; external Gdk3_library;
+function gdk_x11_get_default_xdisplay: PDisplay; cdecl; external Gdk3_library;
+procedure gdk_set_allowed_backends(const backends: PGchar); cdecl; external Gdk3_library;
+{$ENDIF}
 procedure ShowX11Message(const aMessage : string);
 {$ENDIF}{$ENDIF}
 
