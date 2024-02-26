bca50227404e0f2c09b6457c93c2d2c1f4af6acd
yanghy <snxamdf@126.com>
2024-02-26 14:59:31 +0800

    Linux编译问题，去除MyLCL_IniFile.inc
    (cherry picked from commit 1ab8d39c826f4569fedce9d21bcac1b1d8744e19)

--- src/3rd-party/CEF4Delphi/source/uCEFLinuxFunctions.pas
+++ src/3rd-party/CEF4Delphi/source/uCEFLinuxFunctions.pas
@@ -1,40 +1,3 @@
-// ************************************************************************
-// ***************************** CEF4Delphi *******************************
-// ************************************************************************
-//
-// CEF4Delphi is based on DCEF3 which uses CEF to embed a chromium-based
-// browser in Delphi applications.
-//
-// The original license of DCEF3 still applies to CEF4Delphi.
-//
-// For more information about CEF4Delphi visit :
-//         https://www.briskbard.com/index.php?lang=en&pageid=cef
-//
-//        Copyright © 2022 Salvador Diaz Fau. All rights reserved.
-//
-// ************************************************************************
-// ************ vvvv Original license and comments below vvvv *************
-// ************************************************************************
-(*
- *                       Delphi Chromium Embedded 3
- *
- * Usage allowed under the restrictions of the Lesser GNU General Public License
- * or alternatively the restrictions of the Mozilla Public License 1.1
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * Unit owner : Henri Gourvest <hgourvest@gmail.com>
- * Web site   : http://www.progdigy.com
- * Repository : http://code.google.com/p/delphichromiumembedded/
- * Group      : http://groups.google.com/group/delphichromiumembedded
- *
- * Embarcadero Technologies, Inc is not permitted to use or redistribute
- * this source code without explicit permission.
- *
- *)
-
 unit uCEFLinuxFunctions;
 
 {$IFDEF FPC}
@@ -52,7 +15,7 @@ uses
   {$IFDEF LINUX}
     {$IFDEF FPC}
       ctypes, keysym, xf86keysym, x, xlib,
-      {$IFDEF LCLGTK2}gtk2, glib2, gdk2, gtk2proc, gtk2int, Gtk2Def, gdk2x, Gtk2Extra,{$ENDIF}         
+      {$IFDEF LCLGTK2}gtk2, glib2, gdk2, gtk2proc, gtk2int, Gtk2Def, gdk2x, Gtk2Extra,{$ENDIF}
       {$IFDEF LCLGTK3}LazGdk3, LazGtk3, LazGObject2, LazGLib2, gtk3objects, gtk3procs,{$ENDIF}
     {$ENDIF}
   {$ENDIF}
@@ -85,7 +48,7 @@ function gdk_screen_get_resolution(screen:PGdkScreen):gdouble; cdecl; external '
 {$ENDIF}
 {$IFDEF FPC}
 {$IFDEF LCLGTK3}
-function gdk_x11_window_get_xid(window: PGdkWindow): TWindow; cdecl; external Gdk3_library;
+function gdk_x11_window_get_xid(window: PGdkWindow): TXID; cdecl; external Gdk3_library;
 function gdk_x11_get_default_xdisplay: PDisplay; cdecl; external Gdk3_library;
 procedure gdk_set_allowed_backends(const backends: PGchar); cdecl; external Gdk3_library;
 {$ENDIF}
