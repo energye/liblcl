160323501686b49e4be8a80dba54a91f27835a1e
杨红岩 <snxamdf@126.com>
2024-09-25 12:58:02 +0800

    A: add opengl component

--- src/uexport1.pas
+++ src/uexport1.pas
@@ -50,6 +50,8 @@
 {$I MyLCL_RichEdit.inc}  //BASECLASS:TWinControl
 {$I MyLCL_TrackBar.inc}  //BASECLASS:TWinControl
 {$I MyLCL_ImageList.inc}  //BASECLASS:TComponent
+{$I LCL_CustomOpenGLControl.inc}
+{$I LCL_OpenGLControl.inc}
 
 
 //{$ifdef windows}
