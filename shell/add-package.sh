9cf5c7f2794499be042f79efd15e830893e115c2
杨红岩 <snxamdf@126.com>
2024-09-27 21:51:24 +0800

    U: workflows - build test

--- shell/add-package.sh
+++ shell/add-package.sh
@@ -2,18 +2,18 @@
 
 echo "Add Dependency Package"
 
-sudo lazbuild --add-package-link ./src/3rd-party/richmemo/richmemopackage.lpk
-sudo lazbuild ./src/3rd-party/richmemo/richmemopackage.lpk
-sudo lazbuild --add-package ./src/3rd-party/richmemo/ide/richmemo_design.lpk
-sudo lazbuild ./src/3rd-party/richmemo/ide/richmemo_design.lpk
+lazbuild --add-package-link ./src/3rd-party/richmemo/richmemopackage.lpk
+lazbuild ./src/3rd-party/richmemo/richmemopackage.lpk
+lazbuild --add-package ./src/3rd-party/richmemo/ide/richmemo_design.lpk
+lazbuild ./src/3rd-party/richmemo/ide/richmemo_design.lpk
 
-sudo lazbuild --add-package ./src/3rd-party/ATFlatControls/atflatcontrols/atflatcontrols_package.lpk
-sudo lazbuild ./src/3rd-party/ATFlatControls/atflatcontrols/atflatcontrols_package.lpk
+lazbuild --add-package ./src/3rd-party/ATFlatControls/atflatcontrols/atflatcontrols_package.lpk
+lazbuild ./src/3rd-party/ATFlatControls/atflatcontrols/atflatcontrols_package.lpk
 
-sudo lazbuild --add-package-link ./src/3rd-party/DCPcrypt/dcpcrypt.lpk
-sudo lazbuild ./src/3rd-party/DCPcrypt/dcpcrypt.lpk
-sudo lazbuild --add-package ./src/3rd-party/DCPcrypt/dcpcrypt_laz.lpk
-sudo lazbuild ./src/3rd-party/DCPcrypt/dcpcrypt_laz.lpk
+lazbuild --add-package-link ./src/3rd-party/DCPcrypt/dcpcrypt.lpk
+lazbuild ./src/3rd-party/DCPcrypt/dcpcrypt.lpk
+lazbuild --add-package ./src/3rd-party/DCPcrypt/dcpcrypt_laz.lpk
+lazbuild ./src/3rd-party/DCPcrypt/dcpcrypt_laz.lpk
 
-sudo lazbuild --add-package ./src/3rd-party/CEF4Delphi/packages/cef4delphi_lazarus.lpk
-sudo lazbuild ./src/3rd-party/CEF4Delphi/packages/cef4delphi_lazarus.lpk
+lazbuild --add-package ./src/3rd-party/CEF4Delphi/packages/cef4delphi_lazarus.lpk
+lazbuild ./src/3rd-party/CEF4Delphi/packages/cef4delphi_lazarus.lpk
