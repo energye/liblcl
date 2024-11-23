d631de19b5764581cdef22494e450a80c8037d1b
yanghy <snxamdf@126.com>
2024-11-23 22:09:44 +0800

    升级 CEF 130.1.16

--- src/uCEF_LCL_V8InterceptorRef.pas
+++ src/uCEF_LCL_V8InterceptorRef.pas
@@ -13,7 +13,7 @@ interface
 
 uses
   uCEFTypes, uCEFInterfaces, uCEFv8Interceptor,
-  uCEF_LCL_EventCallback;
+  uCEF_LCL_EventCallback, uCEF_LCL_Entity;
 
 type
 
@@ -34,43 +34,6 @@ type
 
 implementation
 
-
-function TV8InterceptorRef.GetByName(const Name: ustring; const object_: ICefv8Value; var retval: ICefv8Value; var Exception: ustring): boolean;
-begin
-  Result := False;
-  if (GetByNamePtr <> nil) then
-  begin
-    TCEFEventCallback.SendEvent(GetByNamePtr, []);
-  end;
-end;
-
-function TV8InterceptorRef.GetByIndex(index: integer; const object_: ICefv8Value; var retval: ICefv8Value; var Exception: ustring): boolean;
-begin
-  Result := False;
-  if (GetByIndexPtr <> nil) then
-  begin
-    TCEFEventCallback.SendEvent(GetByIndexPtr, []);
-  end;
-end;
-
-function TV8InterceptorRef.SetByName(const Name: ustring; const object_, Value: ICefv8Value; var Exception: ustring): boolean;
-begin
-  Result := False;
-  if (SetByNamePtr <> nil) then
-  begin
-    TCEFEventCallback.SendEvent(SetByNamePtr, []);
-  end;
-end;
-
-function TV8InterceptorRef.SetByIndex(index: integer; const object_, Value: ICefv8Value; var Exception: ustring): boolean;
-begin
-  Result := False;
-  if (SetByIndexPtr <> nil) then
-  begin
-    TCEFEventCallback.SendEvent(SetByIndexPtr, []);
-  end;
-end;
-
 constructor TV8InterceptorRef.Create;
 begin
   inherited Create;
@@ -79,6 +42,58 @@ end;
 destructor TV8InterceptorRef.Destroy;
 begin
   inherited Destroy;
+end;
+
+function TV8InterceptorRef.GetByName(const Name: ustring; const object_: ICefv8Value; var retval: ICefv8Value; var Exception: ustring): boolean;
+var
+  RetException: PChar;
+begin
+  Result := False;
+  if (GetByNamePtr <> nil) then
+  begin
+    RetException := PChar(Exception);
+    TCEFEventCallback.SendEvent(GetByNamePtr, [PChar(Name), object_, @retval, @RetException, @Result]);
+    Exception := PCharToUStr(RetException);
+  end;
+end;
+
+function TV8InterceptorRef.GetByIndex(index: integer; const object_: ICefv8Value; var retval: ICefv8Value; var Exception: ustring): boolean;
+var
+  RetException: PChar;
+begin
+  Result := False;
+  if (GetByIndexPtr <> nil) then
+  begin
+    RetException := PChar(Exception);
+    TCEFEventCallback.SendEvent(GetByIndexPtr, [index, object_, @retval, @RetException, @Result]);
+    Exception := PCharToUStr(RetException);
+  end;
+end;
+
+function TV8InterceptorRef.SetByName(const Name: ustring; const object_, Value: ICefv8Value; var Exception: ustring): boolean;
+var
+  RetException: PChar;
+begin
+  Result := False;
+  if (SetByNamePtr <> nil) then
+  begin
+    RetException := PChar(Exception);
+    TCEFEventCallback.SendEvent(SetByNamePtr, [PChar(Name), object_, Value, @RetException, @Result]);
+    Exception := PCharToUStr(RetException);
+  end;
+end;
+
+function TV8InterceptorRef.SetByIndex(index: integer; const object_, Value: ICefv8Value; var Exception: ustring): boolean;
+var
+  RetException: PChar;
+begin
+  Result := False;
+  if (SetByIndexPtr <> nil) then
+  begin
+    RetException := PChar(Exception);
+    TCEFEventCallback.SendEvent(SetByIndexPtr, [index, object_, Value, @RetException, @Result]);
+    Exception := PCharToUStr(RetException);
+  end;
 end;
 
 end.
