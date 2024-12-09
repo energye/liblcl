b8f1a454c24d1a24c067d6d5f9472a34e990f090
yanghy <snxamdf@126.com>
2024-12-09 16:20:17 +0800

    Upgrades 2.5.0

--- src/uexport1.pas
+++ src/uexport1.pas
@@ -50,8 +50,10 @@
 {$I MyLCL_RichEdit.inc}  //BASECLASS:TWinControl
 {$I MyLCL_TrackBar.inc}  //BASECLASS:TWinControl
 {$I MyLCL_ImageList.inc}  //BASECLASS:TComponent
+{$IFnDEF LINUX}
 {$I LCL_CustomOpenGLControl.inc}
 {$I LCL_OpenGLControl.inc}
+{$ENDIF}
 
 
 //{$ifdef windows}
