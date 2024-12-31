5d272b58ba72fb4ecc992be49734b31e0786af83
yanghy <snxamdf@126.com>
2024-12-31 11:50:31 +0800

    A: loong64

--- src/3rd-party/CEF4Delphi/source/uCEFMiscFunctions.pas
+++ src/3rd-party/CEF4Delphi/source/uCEFMiscFunctions.pas
@@ -10,7 +10,7 @@
 // For more information about CEF4Delphi visit :
 //         https://www.briskbard.com/index.php?lang=en&pageid=cef
 //
-//        Copyright © 2023 Salvador Diaz Fau. All rights reserved.
+//        Copyright ï¿½ 2023 Salvador Diaz Fau. All rights reserved.
 //
 // ************************************************************************
 // ************ vvvv Original license and comments below vvvv *************
@@ -1412,8 +1412,8 @@ begin
       {$IFDEF LINUX}
       TempList.Add(TempDir + 'libEGL.so');
       TempList.Add(TempDir + 'libGLESv2.so');
-      TempList.Add(TempDir + 'libvk_swiftshader.so');
-      TempList.Add(TempDir + 'vk_swiftshader_icd.json');
+      //TempList.Add(TempDir + 'libvk_swiftshader.so');
+      //TempList.Add(TempDir + 'vk_swiftshader_icd.json');
       TempList.Add(TempDir + 'libvulkan.so.1');
       {$ENDIF}
       TempList.Add(TempDir + 'icudtl.dat');
